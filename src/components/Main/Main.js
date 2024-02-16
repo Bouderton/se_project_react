432f3b8b30dacbb096d9e31b9c810e159a8bfe52
bouderton <rmkhazal@gmail.com>
2024-02-16 12:56:55 -0800

    weather section

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,11 +3,9 @@ import "./Main.css";
 const Main = () => {
   return (
     <main className="main">
-      <section id="weather">
-        <div>75F</div>
-        <div>
-          <img />
-        </div>
+      <section className="weather">
+        <div className="weather__info">75F</div>
+        <img src="/images/day/day-sunny.svg" className="weather__image" />
       </section>
       <section id="card-section">Card Section</section>
     </main>
