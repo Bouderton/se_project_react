6b45d268d19adfcca700e26db8dc4d363f2c11af
bouderton <rmkhazal@gmail.com>
2024-02-17 18:34:12 -0800

    weather constants moved

--- src/components/util/constants.js
+++ src/components/util/constants.js
@@ -36,3 +36,66 @@ export const defaultClothingItems = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Coat.png?etag=298717ed89d5e40b1954a1831ae0bdd4",
   },
 ];
+
+export const weatherOptions = [
+  {
+    url: require("../../images/day/day-sunny.svg").default,
+    day: true,
+    type: "sunny",
+  },
+  {
+    url: require("../../images/day/day-cloudy.svg").default,
+    day: true,
+    type: "cloudy",
+  },
+  {
+    url: require("../../images/day/day-rain.svg").default,
+    day: true,
+    type: "rainy",
+  },
+  {
+    url: require("../../images/day/day-storm.svg").default,
+    day: true,
+    type: "storm",
+  },
+  {
+    url: require("../../images/day/day-snow.svg").default,
+    day: true,
+    type: "snowy",
+  },
+  {
+    url: require("../../images/day/day-fog.svg").default,
+    day: true,
+    type: "fog",
+  },
+  {
+    url: require("../../images/night/night-moon.svg").default,
+    day: false,
+    type: "moon",
+  },
+  {
+    url: require("../../images/night/night-cloudy.svg").default,
+    day: false,
+    type: "cloudy",
+  },
+  {
+    url: require("../../images/night/night-rain.svg").default,
+    day: false,
+    type: "rainy",
+  },
+  {
+    url: require("../../images/night/night-storm.svg").default,
+    day: false,
+    type: "storm",
+  },
+  {
+    url: require("../../images/night/night-snow.svg").default,
+    day: false,
+    type: "snowy",
+  },
+  {
+    url: require("../../images/night/night-fog.svg").default,
+    day: false,
+    type: "fog",
+  },
+];
