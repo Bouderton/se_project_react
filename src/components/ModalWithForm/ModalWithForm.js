6beed049f36f008c626c4481c05d322944f90e08
bouderton <rmkhazal@gmail.com>
2024-02-18 10:35:30 -0800

    ModalWithForm

---
+++ src/components/ModalWithForm/ModalWithForm.js
@@ -0,0 +1,33 @@
+import "./ModalWithForm.css";
+
+const ModalWithForm = ({
+  children,
+  buttonText = "Add Garmet",
+  title,
+  onClose,
+  name,
+}) => {
+  return (
+    <div className={`modal modal__type_${name}`}>
+      <div className="form__content">
+        <button type="button" onClick={onClose} />
+        <h3>{title}</h3>
+        <form className="form">
+          {children}
+          <button type="submit">{buttonText}</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ModalWithForm;
+
+/* <fieldset className="form__fieldset">
+          <input placeholder="Name" type="text">
+            Name
+          </input>
+          <input placeholder="Image URL" type="url">
+            Image URL
+          </input>
+        </fieldset> */
