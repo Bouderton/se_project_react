905afbfad249f276588f2a4b87ac3906b042a8d7
bouderton <rmkhazal@gmail.com>
2024-02-20 21:46:51 -0800

    logice before weather API

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,8 +2,20 @@ import "./Main.css";
 import { defaultClothingItems } from "../util/constants";
 import WeatherCard from "../WeatherCard/WeatherCard";
 import ItemCard from "../ItemCard/ItemCard";
+import { useMemo } from "react";
 
 function Main({ weatherTemp, onSelectCard }) {
+  // const weatherType = useMemo(() => {
+  //   if (weatherTemp >= 86) {
+  //     return "hot";
+  //   } else if (weatherTemp >= 66 && weatherTemp <= 85) {
+  //     return "warm";
+  //   } else if (weatherTemp <= 65) {
+  //     return "cold";
+  //   }
+  // }, [weatherTemp]);
+
+  console.log(weatherType);
   return (
     <main className="main">
       <WeatherCard day={true} type="cloudy" weatherTemp={weatherTemp} />
