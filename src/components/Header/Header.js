2b2c1522755f3aa3d9db43ee9abc9465b615ff62
bouderton <rmkhazal@gmail.com>
2024-02-25 15:00:27 -0800

    current date obj

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,13 +3,18 @@ import avatar from "../../images/avatar-logo.svg";
 import logo from "../../images/wtwr.svg";
 
 const Header = ({ onCreateModal }) => {
+  const currentDate = new Date().toLocaleString("default", {
+    month: "long",
+    day: "numeric",
+  });
+
   return (
     <header className="header">
       <div className="header__logo">
         <div>
           <img src={logo} alt="logo" />
         </div>
-        <div>Feburary 25, Las Vegas</div>
+        <div>{currentDate}, Las Vegas</div>
       </div>
       <div className="header__avatar-logo">
         <div>
