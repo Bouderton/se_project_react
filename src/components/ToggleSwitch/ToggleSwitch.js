5c14a0901bfb9308b9aba87ea5738641fea9f900
bouderton <rmkhazal@gmail.com>
2024-03-14 21:16:01 -0700

    temp switching

--- src/components/ToggleSwitch/ToggleSwitch.js
+++ src/components/ToggleSwitch/ToggleSwitch.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import "./ToggleSwitch.css";
 
 const ToggleSwitch = () => {
+  const [currentTempUnit, handleToggleSwitch] = useState("C");
+
+  const handleChange = (e) => {
+    if (currentTempUnit === "C") handleToggleSwitch("F");
+    if (currentTempUnit === "F") handleToggleSwitch("C");
+  };
+
+  console.log(currentTempUnit);
+
   return (
-    <label>
-      <input type="checkbox"></input>
+    <label className="switch">
+      <input type="checkbox" className="switch__box" onChange={handleChange} />
+      <span></span>
+      <p>F</p>
+      <p>C</p>
     </label>
   );
 };
