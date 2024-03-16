5d2c1fce16460596bc5e4afbc38e2a41d9e50a2c
bouderton <rmkhazal@gmail.com>
2024-03-16 16:02:30 -0700

    added json server

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,20 +1,34 @@
 const baseUrl = "http://localhost:3001";
 
 const checkResponse = (res) => {
-    return res.ok ? res.json(): Promise.reject(`Error ${res.status}`);
-}
+  return res.ok ? res.json() : Promise.reject(`Error ${res.status}`);
+};
 
-const getItems = () => { 
-    return fetch(`${baseUrl}/items`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        }
-    }).then((res) => (checkResponse(res))); 
-}
+const getItems = () => {
+  return fetch(`${baseUrl}/items`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => checkResponse(res));
+};
+
+const postItems = () => {
+  return fetch(`${baseUrl}/items`, {
+    method: "POST",
+  }).then((res) => checkResponse(res));
+};
+
+const deleteItems = () => {
+  return fetch(`${baseUrl}/items}`, {
+    method: "DELETE",
+  }).then((res) => checkResponse(res));
+};
 
 const api = {
-    getItems,
-}
+  getItems,
+  postItems,
+  deleteItems,
+};
 
 export default api;
