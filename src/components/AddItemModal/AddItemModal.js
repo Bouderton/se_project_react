6817ca025ef5dadd91d844df91c85ea80c4e8bd3
bouderton <rmkhazal@gmail.com>
2024-03-17 12:05:13 -0700

    additem event handlers

--- src/components/AddItemModal/AddItemModal.js
+++ src/components/AddItemModal/AddItemModal.js
@@ -1,10 +1,34 @@
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
+import { useState, useEffect } from "react";
 
 const AddItemModal = ({ isOpen, handleAddItem, handleCloseModal }) => {
   // declare state for each input field
+  const [name, setName] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [temp, setTemp] = useState("");
 
   // use a useEffect hook to reset the input field state to empty strings when
   // the modal is opened
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleUrlChange = (e) => {
+    setImageUrl(e.target.value);
+  };
+
+  const handleTempChange = (e) => {
+    setTemp(e.target.value);
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      setName("");
+      setImageUrl("");
+      setTemp("");
+    }
+  });
 
   // create onChange handlers corresponding to each state variable
 
@@ -12,6 +36,7 @@ const AddItemModal = ({ isOpen, handleAddItem, handleCloseModal }) => {
     // prevent default behavior
     e.preventDefault();
     // call onAddItem with appropriate arguments
+    handleAddItem({ name, imageUrl });
   }
 
   //  Pass appropiate props to ModalWithForm
@@ -32,6 +57,7 @@ const AddItemModal = ({ isOpen, handleAddItem, handleCloseModal }) => {
           maxLength="30"
           placeholder="Name"
           className="modal__form-input"
+          onChange={handleNameChange}
         />
       </label>
       <label className="modal__form-label">
@@ -42,20 +68,39 @@ const AddItemModal = ({ isOpen, handleAddItem, handleCloseModal }) => {
           placeholder="Image URL"
           minLength="1"
           className="modal__form-input"
+          onChange={handleUrlChange}
         />
       </label>
       <div className="modal__weather-type">
         <p className="modal__weather-title">Select Weather Type</p>
         <div>
-          <input name="weather" type="radio" id="hot" value="hot" />
+          <input
+            name="weather"
+            type="radio"
+            id="hot"
+            value="hot"
+            onChange={handleTempChange}
+          />
           <label className="modal__weather-option">Hot</label>
         </div>
         <div>
-          <input name="weather" type="radio" id="warm" value="warm" />
+          <input
+            name="weather"
+            type="radio"
+            id="warm"
+            value="warm"
+            onChange={handleTempChange}
+          />
           <label className="modal__weather-option">Warm</label>
         </div>
         <div>
-          <input name="weather" type="radio" id="cold" value="cold" />
+          <input
+            name="weather"
+            type="radio"
+            id="cold"
+            value="cold"
+            onChange={handleTempChange}
+          />
           <label className="modal__weather-option">Cold</label>
         </div>
       </div>
