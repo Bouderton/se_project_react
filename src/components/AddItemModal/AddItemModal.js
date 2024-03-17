77698335cf9d7a081cf43ad211f4aed4874a1dcf
bouderton <rmkhazal@gmail.com>
2024-03-17 10:42:35 -0700

    dumpster fire :D

--- src/components/AddItemModal/AddItemModal.js
+++ src/components/AddItemModal/AddItemModal.js
@@ -1,6 +1,6 @@
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
 
-const AddItemModal = ({ isOpen, onAddItem, onCloseModal }) => {
+const AddItemModal = ({ isOpen, handleAddItem, handleCloseModal }) => {
   // declare state for each input field
 
   // use a useEffect hook to reset the input field state to empty strings when
@@ -12,12 +12,53 @@ const AddItemModal = ({ isOpen, onAddItem, onCloseModal }) => {
     // prevent default behavior
     e.preventDefault();
     // call onAddItem with appropriate arguments
-    onAddItem();
   }
 
   //  Pass appropiate props to ModalWithForm
   //  ModalWithForm
-  <ModalWithForm></ModalWithForm>;
+  <ModalWithForm
+    title="New Garmet"
+    onClose={handleCloseModal}
+    buttonText="Add garment"
+    isOpen={isOpen}
+  >
+    <label className="modal__form-label">
+      Name
+      <input
+        type="text"
+        name="name"
+        minLength="1"
+        maxLength="30"
+        placeholder="Name"
+        className="modal__form-input"
+      />
+    </label>
+    <label className="modal__form-label">
+      Image
+      <input
+        type="url"
+        name="link"
+        placeholder="Image URL"
+        minLength="1"
+        className="modal__form-input"
+      />
+    </label>
+    <div className="modal__weather-type">
+      <p className="modal__weather-title">Select Weather Type</p>
+      <div>
+        <input name="weather" type="radio" id="hot" value="hot" />
+        <label className="modal__weather-option">Hot</label>
+      </div>
+      <div>
+        <input name="weather" type="radio" id="warm" value="warm" />
+        <label className="modal__weather-option">Warm</label>
+      </div>
+      <div>
+        <input name="weather" type="radio" id="cold" value="cold" />
+        <label className="modal__weather-option">Cold</label>
+      </div>
+    </div>
+  </ModalWithForm>;
   //    Form contents
 };
 
