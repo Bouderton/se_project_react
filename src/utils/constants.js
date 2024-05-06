f07e3bef4b8331f79b3320ee192bbea191392bc5
bouderton <rmkhazal@gmail.com>
2024-05-06 15:15:33 -0700

    i think this works

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,61 +1,61 @@
 export const weatherOptions = [
   {
-    url: require("../images/day/day-sunny.svg").default,
+    url: "../images/day/day-sunny.svg".default,
     day: true,
     type: "sunny",
   },
   {
-    url: require("../images/day/day-cloudy.svg").default,
+    url: "../images/day/day-cloudy.svg".default,
     day: true,
     type: "cloudy",
   },
   {
-    url: require("../images/day/day-rain.svg").default,
+    url: "../images/day/day-rain.svg".default,
     day: true,
     type: "rainy",
   },
   {
-    url: require("../images/day/day-storm.svg").default,
+    url: "../images/day/day-storm.svg".default,
     day: true,
     type: "storm",
   },
   {
-    url: require("../images/day/day-snow.svg").default,
+    url: "../images/day/day-snow.svg".default,
     day: true,
     type: "snowy",
   },
   {
-    url: require("../images/day/day-fog.svg").default,
+    url: "../images/day/day-fog.svg".default,
     day: true,
     type: "fog",
   },
   {
-    url: require("../images/night/night-moon.svg").default,
+    url: "../images/night/night-moon.svg".default,
     day: false,
     type: "moon",
   },
   {
-    url: require("../images/night/night-cloudy.svg").default,
+    url: "../images/night/night-cloudy.svg".default,
     day: false,
     type: "cloudy",
   },
   {
-    url: require("../images/night/night-rain.svg").default,
+    url: "../images/night/night-rain.svg".default,
     day: false,
     type: "rainy",
   },
   {
-    url: require("../images/night/night-storm.svg").default,
+    url: "../images/night/night-storm.svg".default,
     day: false,
     type: "storm",
   },
   {
-    url: require("../images/night/night-snow.svg").default,
+    url: "../images/night/night-snow.svg".default,
     day: false,
     type: "snowy",
   },
   {
-    url: require("../images/night/night-fog.svg").default,
+    url: "../images/night/night-fog.svg".default,
     day: false,
     type: "fog",
   },
