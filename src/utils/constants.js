6f161de72dfdbd4dc917625ebf5c0b46b11c4bb3
bouderton <rmkhazal@gmail.com>
2024-05-06 23:36:30 -0700

    image paths not working | possibly the files themselves

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,61 +1,61 @@
 export const weatherOptions = [
   {
-    url: "../images/day/day-sunny.svg",
+    url: "/se_project_react/src/images/day/day-sunny.svg",
     day: true,
     type: "sunny",
   },
   {
-    url: "../images/day/day-cloudy.svg",
+    url: "/se_project_react/src/images/day/day-cloudy.svg",
     day: true,
     type: "cloudy",
   },
   {
-    url: "../images/day/day-rain.svg",
+    url: "/se_project_react/src/images/day/day-rain.svg",
     day: true,
     type: "rainy",
   },
   {
-    url: "../images/day/day-storm.svg",
+    url: "/se_project_react/src/images/day/day-storm.svg",
     day: true,
     type: "storm",
   },
   {
-    url: "../images/day/day-snow.svg",
+    url: "/se_project_react/src/images/day/day-snow.svg",
     day: true,
     type: "snowy",
   },
   {
-    url: "../images/day/day-fog.svg",
+    url: "/se_project_react/src/images/day/day-fog.svg",
     day: true,
     type: "fog",
   },
   {
-    url: "../images/night/night-moon.svg",
+    url: "/se_project_react/src/images/night/night-moon.svg",
     day: false,
     type: "moon",
   },
   {
-    url: "../images/night/night-cloudy.svg",
+    url: "/se_project_react/src/images/night/night-cloudy.svg",
     day: false,
     type: "cloudy",
   },
   {
-    url: "../images/night/night-rain.svg",
+    url: "/se_project_react/src/images/night/night-rain.svg",
     day: false,
     type: "rainy",
   },
   {
-    url: "../images/night/night-storm.svg",
+    url: "/se_project_react/src/images/night/night-storm.svg",
     day: false,
     type: "storm",
   },
   {
-    url: "../images/night/night-snow.svg",
+    url: "/se_project_react/src/images/night/night-snow.svg",
     day: false,
     type: "snowy",
   },
   {
-    url: "../images/night/night-fog.svg",
+    url: "/se_project_react/src/images/night/night-fog.svg",
     day: false,
     type: "fog",
   },
