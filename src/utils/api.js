a4d2a454874fe2ab1bc06d93454ca904d5058f3c
bouderton <rmkhazal@gmail.com>
2024-05-07 13:04:54 -0700

    json back installed (why)

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ export const checkResponse = (res) => {
   return res.ok ? res.json() : Promise.reject(`Error ${res.status}`);
 };
 
-const baseUrl = "http://localhost:3000";
+const baseUrl = "http://localhost:3001";
 
 const getItems = () => {
   return fetch(`${baseUrl}/items`, {
