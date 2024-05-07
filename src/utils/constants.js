c1cce16f704b55f73ec90d04607c2b9f032f9afb
bouderton <rmkhazal@gmail.com>
2024-05-06 23:28:31 -0700

    trying to fix images and api calls

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,61 +1,61 @@
 export const weatherOptions = [
   {
-    url: "../images/day/day-sunny.svg".default,
+    url: "../images/day/day-sunny.svg",
     day: true,
     type: "sunny",
   },
   {
-    url: "../images/day/day-cloudy.svg".default,
+    url: "../images/day/day-cloudy.svg",
     day: true,
     type: "cloudy",
   },
   {
-    url: "../images/day/day-rain.svg".default,
+    url: "../images/day/day-rain.svg",
     day: true,
     type: "rainy",
   },
   {
-    url: "../images/day/day-storm.svg".default,
+    url: "../images/day/day-storm.svg",
     day: true,
     type: "storm",
   },
   {
-    url: "../images/day/day-snow.svg".default,
+    url: "../images/day/day-snow.svg",
     day: true,
     type: "snowy",
   },
   {
-    url: "../images/day/day-fog.svg".default,
+    url: "../images/day/day-fog.svg",
     day: true,
     type: "fog",
   },
   {
-    url: "../images/night/night-moon.svg".default,
+    url: "../images/night/night-moon.svg",
     day: false,
     type: "moon",
   },
   {
-    url: "../images/night/night-cloudy.svg".default,
+    url: "../images/night/night-cloudy.svg",
     day: false,
     type: "cloudy",
   },
   {
-    url: "../images/night/night-rain.svg".default,
+    url: "../images/night/night-rain.svg",
     day: false,
     type: "rainy",
   },
   {
-    url: "../images/night/night-storm.svg".default,
+    url: "../images/night/night-storm.svg",
     day: false,
     type: "storm",
   },
   {
-    url: "../images/night/night-snow.svg".default,
+    url: "../images/night/night-snow.svg",
     day: false,
     type: "snowy",
   },
   {
-    url: "../images/night/night-fog.svg".default,
+    url: "../images/night/night-fog.svg",
     day: false,
     type: "fog",
   },
