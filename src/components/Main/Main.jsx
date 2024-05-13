38f7a6fa6dceb81505ea3e9f9a9256ea9775d733
bouderton <rmkhazal@gmail.com>
2024-05-13 11:32:26 -0700

    owner and weather card filters

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,9 +4,11 @@ import ItemCard from "../ItemCard/ItemCard";
 import WeatherCard from "../WeatherCard/WeatherCard";
 import { useMemo, useContext } from "react";
 import { CurrentTempUnitContext } from "../../contexts/CurrentTemperatureUnitContext";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
 function Main({ weatherTemp, onSelectCard, clothingItems }) {
   const { currentTempUnit } = useContext(CurrentTempUnitContext);
+  const currentUser = useContext(CurrentUserContext);
 
   const temp = weatherTemp?.temperature?.[currentTempUnit] || 999;
   const weatherType = useMemo(() => {
@@ -33,9 +35,19 @@ function Main({ weatherTemp, onSelectCard, clothingItems }) {
       <section className="card__section" id="card-section">
         Today is {`${temp}°${currentTempUnit}`} / You may want to wear:
         <div className="card__items">
-          {filteredCards.map((item) => (
-            <ItemCard key={item._id} item={item} onSelectCard={onSelectCard} />
-          ))}
+          {filteredCards.map((item) => {
+            const isOwn = item.owner === currentUser._id;
+            console.log(item.owner, currentUser._id);
+            return (
+              isOwn && (
+                <ItemCard
+                  key={item._id}
+                  item={item}
+                  onSelectCard={onSelectCard}
+                />
+              )
+            );
+          })}
         </div>
       </section>
     </main>
