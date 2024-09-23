898f0eb9ae6d75fee6751ce365b4a49217b20d6d
bouderton <rmkhazal@gmail.com>
2024-09-22 17:43:11 -0700

    mobile header start

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -23,64 +23,70 @@ const Header = ({
 
   return (
     <header className="header">
-      <div className="header__logo">
-        <div>
-          <Link to="/">
-            <img src={logo} alt="logo" />
-          </Link>
+      <div className="header__content">
+        <div className="header__logo">
+          <div>
+            <Link to="/">
+              <img src={logo} alt="logo" />
+            </Link>
+          </div>
+          <div>
+            {currentDate}, {location}
+          </div>
         </div>
-        <div>
-          {currentDate}, {location}
-        </div>
-      </div>
-      <div className="header__info-container">
-        <ToggleSwitch />
-        {isLoggedIn ? (
-          <>
-            <div>
+        <div className="header__info-container">
+          <ToggleSwitch />
+          {isLoggedIn ? (
+            <>
+              <div>
+                <button
+                  type="text"
+                  className="header__button"
+                  onClick={onCreateModal}
+                >
+                  +Add New Clothes
+                </button>
+              </div>
+              <Link
+                to="/profile"
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                {currentUser?.name}
+              </Link>
+              <div>
+                <img
+                  src={currentUser?.avatar}
+                  alt="avatar logo"
+                  className="header__avatar-img"
+                />
+              </div>
+            </>
+          ) : (
+            <>
               <button
+                onClick={handleSignUpModal}
                 type="text"
                 className="header__button"
-                onClick={onCreateModal}
               >
-                +Add New Clothes
+                Sign Up
               </button>
-            </div>
-            <Link
-              to="/profile"
-              style={{ textDecoration: "none", color: "black" }}
-            >
-              {currentUser?.name}
-            </Link>
-            <div>
-              <img
-                src={currentUser?.avatar}
-                alt="avatar logo"
-                className="header__avatar-img"
-              />
-            </div>
-          </>
-        ) : (
-          <>
-            <button
-              onClick={handleSignUpModal}
-              type="text"
-              className="header__button"
-            >
-              Sign Up
-            </button>
-            <button
-              onClick={handleLoginModal}
-              type="text"
-              className="header__button"
-            >
-              Log In
-            </button>
-          </>
-        )}
-        <div className="header__mobile-container">
-          <HeaderMobile />
+              <button
+                onClick={handleLoginModal}
+                type="text"
+                className="header__button"
+              >
+                Log In
+              </button>
+            </>
+          )}
         </div>
+      </div>
+      <div className="header__mobile-container">
+        <HeaderMobile
+          isLoggedIn={isLoggedIn}
+          location={location}
+          currentDate={currentDate}
+        />
       </div>
     </header>
   );
--- src/components/HeaderMobile/HeaderMobile.jsx
+++ src/components/HeaderMobile/HeaderMobile.jsx
@@ -1,11 +1,34 @@
 import "../Header/Header.css";
 import logo from "../../images/wtwr.svg";
+import mobileicon from "../../images/mobileicon.svg";
 import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
+import { Link } from "react-router-dom";
 
-const HeaderMobile = () => {
+const HeaderMobile = ({ isLoggedIn, location, currentDate }) => {
   return (
     <>
-      <h1>BALLS</h1>
+      <header className="header mobile">
+        <div className="header__mobile-info">
+          <div className="header__logo">
+            <Link to="/">
+              <img src={logo} alt="logo" />
+            </Link>
+          </div>
+          <div>
+            {currentDate}, {location}
+          </div>
+        </div>
+        <div className="header__info-container">
+          <ToggleSwitch />
+          <button className="header__menu-icon" type="button">
+            <img
+              className="header__menu-icon-image"
+              alt="menu icon"
+              src={mobileicon}
+            />
+          </button>
+        </div>
+      </header>
     </>
   );
 };
