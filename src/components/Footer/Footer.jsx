91cf7234a786094abcc99136912cedf32f0763a8
bouderton <rmkhazal@gmail.com>
2024-09-24 12:44:59 -0700

    new footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,31 @@
 import "./Footer.css";
+import github from "../../images/github.svg";
+import linkedin from "../../images/linkedin.svg";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div>Developed by Ryan Khazal</div>
-      <div>2024</div>
+      <div>Developed by Ryan Khazal 2024</div>
+      <div className="footer__info-container">
+        <a
+          rel="noopener noreferer"
+          target="_blank"
+          href="https://www.linkedin.com/in/ryankhazal/"
+        >
+          <img
+            src={linkedin}
+            alt="LinkedIn Icon"
+            className="footer__info-icon"
+          />
+        </a>
+        <a
+          rel="noopener noreferer"
+          target="_blank"
+          href="https://github.com/Bouderton"
+        >
+          <img src={github} alt="Github Icon" className="footer__info-icon" />
+        </a>
+      </div>
     </footer>
   );
 };
